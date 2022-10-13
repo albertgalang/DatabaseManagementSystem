7c7ac5511d512fc7b9bc88d9394895c87d264149
Albert Galang <albrtglng@gmail.com>
2022-10-13 13:11:17 -0700

    Read command/query inputs from file.

---
+++ Application/Variables.py
@@ -0,0 +1,4 @@
+import os
+
+# current working directory is always where Main.py resides.
+DBMS_PATH = f"{os.getcwd()}/DBMS"
--- Main.py
+++ Main.py
@@ -1,4 +1,6 @@
 import sys
+from fileinput import filename
+
 from Parser import *
 from Executer import Executer
 
@@ -6,28 +8,61 @@ from Executer import Executer
 # Main entry of the application
 # determines the queries and runs them
 def main(argv):
+    # Load the program
+    # - variables
+    # - make sure DBMS folder exists
+    # - settings
+    # - parser
     settings = {"database": None}
-    while True:
-        inp = input()
+    parser = Parser()
+    execute = Executer()
 
-        if inp.strip() == ".EXIT":
-            print("\nAll done")
-            break
-        if inp.strip()[0:2] == "--":
-            continue
+    # reading inputs from file
+    # inputs is a list that includes the ';'
+    file_detected = len(argv) > 1
+    if file_detected:
+        file_path = argv[1]
+        try:
 
-        parser = Parser()
-        executer = Executer()
-        for line in inp.strip().split(';')[:-1]:
-            inp_line = line + ";"
-            tokens = parser.tokenize(inp_line.lower())
-            # print(tokens)
-            if parser.validate_syntax(tokens) and parser.validate_query(tokens):
-                query = executer.generate_context(tokens, settings)
-                executer.run(query)
-            else:
-                print("!Error: invalid syntax detected.")
+            with open(file_path, 'r') as file:
+                inputs = file.readlines()
+
+        except filename:
+
+            print("Invalid file path.")
+
+        else:
+
+            for inp in inputs:
+                if parser.validate_syntax(inp):
+                    pass
+
+    # manual command/query input in the terminal
+    else:
+        while True:
+            inputs = input()
+
+    # while True:
+    #     inp = input()
+    #
+    #     if inp.strip() == ".EXIT":
+    #         print("\nAll done")
+    #         break
+    #     if inp.strip()[0:2] == "--":
+    #         continue
+    #
+    #     parser = Parser()
+    #     executer = Executer()
+    #     for line in inp.strip().split(';')[:-1]:
+    #         inp_line = line + ";"
+    #         tokens = parser.tokenize(inp_line.lower())
+    #         # print(tokens)
+    #         if parser.validate_syntax(tokens) and parser.validate_query(tokens):
+    #             query = executer.generate_context(tokens, settings)
+    #             executer.run(query)
+    #         else:
+    #             print("!Error: invalid syntax detected.")
 
 
 if __name__ == "__main__":
-    main(sys.argv[0])
+    main(sys.argv)
